cffaf20f19d639840b0685ae16f142bdbb37ff21
AndrewFan0408 <andrewfan0408@gmail.com>
2023-01-12 19:43:53 -0800

    Pass 10

--- src/recursion.js
+++ src/recursion.js
@@ -130,6 +130,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase().split(' ').join('');
+  if (string.length <= 1) {
+    return true;
+  }
+  if (string.charAt(0) !== string.charAt(string.length - 1)) {
+    return false;
+  }
+  return palindrome(string.slice(1, string.length - 1));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
