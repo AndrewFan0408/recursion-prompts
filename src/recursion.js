99148b64cdc842c3ef00d06964db6daa3ced0e17
AndrewFan0408 <andrewfan0408@gmail.com>
2023-01-12 18:36:56 -0800

    Pass 9

--- src/recursion.js
+++ src/recursion.js
@@ -122,6 +122,10 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length <= 1) {
+    return string;
+  }
+  return reverse(string.slice(1)) + string.charAt(0);
 };
 
 // 10. Write a function that determines if a string is a palindrome.
